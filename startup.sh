53b02bad63a2011e75e5dc9eb4b36e244a730e69
Grzegorz Pietrzak <grzegorz@gpietrzak.pl>
2020-04-18 17:01:37 +0200

    Startup.sh: added upgrade and usefull software

---
+++ startup.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# update repositories informations
+sudo apt-get update
+
+# upgrade all packeges installed on your system
+sudo apt-get upgrade
+
+
+sudo apt-get install mc
+sudo apt-get install filezilla
+sudo apt-get install vlc
+
+
+
+sudo add-apt-repository ppa:phoerious/keepassxc
+sudo apt-get update
+sudo apt-get install keepassxc
