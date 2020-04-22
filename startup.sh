070df7d1e2214470f2f7d02879baf5b9ad3215ce
gpietrzak-pl <kontakt@gpietrzak.pl>
2020-04-22 18:57:56 +0200

    Changed apt-get to apt

--- startup.sh
+++ startup.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
 
 # update repositories informations
-sudo apt-get update
+sudo apt update
 
 # upgrade all packeges installed on your system
-sudo apt-get upgrade
+sudo apt upgrade
 
 
-sudo apt-get install mc
-sudo apt-get install filezilla
-sudo apt-get install vlc
+sudo apt install mc
+sudo apt install filezilla
+sudo apt install vlc
 
 
 
 sudo add-apt-repository ppa:phoerious/keepassxc
-sudo apt-get update
-sudo apt-get install keepassxc
+sudo apt update
+sudo apt install keepassxc
